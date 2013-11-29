66fe6331070654efc9426528f6a9a5dfd76ae465
cahgantcore <m.sena.luphdika@gmail.com>
2013-11-29 09:30:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+?>
+
+<a href="csv.php">Ambil data format CSV</a>
+<br>
+<a href="sql.php">Ambil data format SQL</a>
+<br>
+<a href="xml.php">Ambil data format XML</a>
+<br>
+<br>
+<a href="others.php">Ambil dari data tim lain!</a>
+
+<p>
+        M Sena Luphdika                 18211009
+        <br>
+        Kurniawan Adhi Ramdhani 18211024
+</p>
